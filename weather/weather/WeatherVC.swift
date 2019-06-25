7cbe1b495bb2b935cc5304aa3b3eb7239046cce3
Vladyslav Kolomiets <vladyslavkolomiets@gmail.com>
2019-06-25 17:17:45 +0300

    added basic UI

---
+++ weather/weather/WeatherVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Vladyslav Kolomiets on 6/25/19.
+//  Copyright © 2019 Vladyslav Kolomiets. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var currentTempLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var currentWeatherImage: UIImageView!
+  @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+  @IBOutlet weak var tableview: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+}
+
